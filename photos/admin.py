73ca653abbe5b064166d5acce06f7346b06942d1
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-01-11 05:28:10 +0300

    register models function

--- photos/admin.py
+++ photos/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 from .models import Tag, Location, Image
 
-# Register your models here.
-admin.site.register(Tag)
-admin.site.register(Location)
-admin.site.register(Image)
+# # Register your models here.
+# admin.site.register(Tag)
+# admin.site.register(Location)
+# admin.site.register(Image)
 
 
